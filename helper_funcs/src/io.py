29b9ff0a197a9d2fcf0a285a353fa0f372e2acf9
Peter Pezon <pezon@uchicago.edu>
2023-05-22 17:01:07 -0500

    feat: added evaluate_policy method to evaluate a policy in training

--- helper_funcs/src/io.py
+++ helper_funcs/src/io.py
@@ -3,6 +3,7 @@ import shutil
 from io import BytesIO
 from zipfile import ZipFile
 
+import numpy as np
 import tensorflow as tf
 try:
     from google.colab import files
@@ -52,10 +53,32 @@ def save_policy_tflite(policy_dir, save_dir, race):
     See [TensorFlow Lite converter](https://www.tensorflow.org/lite/convert) for more details.
     """
     save_dir.mkdir(exist_ok=True, parents=True)
-    converter = tf.lite.TFLiteConverter.from_saved_model(
-        str(policy_dir), signature_keys=["action"])
+    converter = tf.lite.TFLiteConverter.from_saved_model(str(policy_dir))
     tflite_policy = converter.convert()
     exported_qnet_tflite = save_dir / POLICY_TFLITE_FILE.format(race=race)
     with open(exported_qnet_tflite, "wb") as f:
         f.write(tflite_policy)
     return exported_qnet_tflite
+
+
+def evaluate_policy(tf_env, py_env, policy, num_episodes = 3):
+    total_return = 0.0
+    for i in range(num_episodes):
+        episode_return = 0.0
+        time_step = tf_env.reset()
+        lap = 1
+        while not time_step.is_last():
+            action_step = policy.action(time_step)
+            time_step = tf_env.step(action_step.action)
+            episode_return += np.mean(time_step.reward)
+            print(f"race {i + 1}: driver = {py_env.idx_driver}, lap = {lap}, action = {action_step}")
+            lap += 1
+        total_return += episode_return
+        final_position = py_env.race.positions[
+           py_env.race.get_last_compl_lap(py_env.idx_driver),
+           py_env.idx_driver
+        ]
+        print(f"race {i + 1}: driver = {py_env.idx_driver}, final_position = {final_position}")
+    average_return = total_return / num_episodes
+    return average_return
+ 
